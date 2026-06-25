6303d049253d450f83de2123491f10bd05d41bde
ankitkujur15 <ankitujjwalkujur14@gmail.com>
2026-06-25 16:18:47 +0530

    Add Python chapter files

---
+++ chap2.py
@@ -0,0 +1,57 @@
+#this line is a comment
+print("hello boi!!!")
+
+"""comments are notes
+you write in your code to explain what it does           --> mulitline comments
+python completely ignores them --- they dont affect your code"""
+
+#Variables are containers for storing data values
+
+sher = 12
+print(sher)
+
+# anything can become a variable
+
+"""  but there are certain rules to follow when naming variables 
+    ❌  1name = "x"  → can't start with a number
+    ❌  my name = "x"  → no spaces allowed
+    ❌  my-name = "x"  → no special characters (except underscore)
+         
+"""
+
+# Naming conventions for variables
+
+"""
+1. camel Case: myVariableName
+     sheriansXNyc = 12
+
+2. Pascal Case: MyVariableName
+     SheriansXNyc = 23
+
+3. Snake Case: my_variable_name
+     sherians_x_nyc = 34             // python prefers to this one
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chap3.py
@@ -0,0 +1,34 @@
+
+# data types in python
+
+"""
+  * You don't need to declare  the data type -- python automatically assignes the data type based on the value you assigne to the variable.
+
+
+"""
+#int
+a = -34
+print(type(a))   # <class 'int'>
+
+#float
+sher = 3.5
+print(type(sher))   # <class 'float'>
+
+#complex
+c = 12 + 3j
+print(type(c))   # <class 'complex'>   j -> represents the imaginary part of a complex number
+
+#string
+str = 'sherians is good for "frontend" development'
+print(str)         # <class 'string'>
+
+#boolean
+
+a = True
+b = False
+
+print(type(a))   # <class 'bool'>
+print(type(b))   # <class 'bool'>
+
+
+# class type() is used to check the data type of a  variable.
