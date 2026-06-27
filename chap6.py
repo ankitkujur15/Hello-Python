61cb6b7afadc0f2de7ca8a9eb4f309bd21761e22
ankitkujur15 <ankitujjwalkujur14@gmail.com>
2026-06-27 18:29:54 +0530

    update for day 3

--- chap6.py
+++ chap6.py
@@ -1,2 +1,43 @@
 # Conditional statements in Python
 
+# they make decisions based on certain conditions.
+
+
+# if condition:      -> you have one condition to check
+
+
+# if True:
+#     print("This is true")  # → This is true
+
+# #take input of age from user and check if the user is eligible to vote or not
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+  #---------------------------------------------------------------------------  
+
+# if-while-else statement 
+
+# rupees = int(input('give  money -'))
+# if rupees  == 10:
+#     print('you can buy a chocolate')
+# elif rupees == 20:
+#     print('you can buy a burger')
+# else:
+#     print('bukha marega ga tu')
+
+# que 1
+
+num1 = int(input('1st no - '))
+num2 =int(input('2nd no - '))
+
+if num1 > num2:
+    print('1st no is greater')
+elif num1 < num2:
+    print('2nd no is greater')
+else:
+    print('both are equal')
+
