4b10e2c1b900103406dd5d72d2cc410521ce7aed
ankitkujur15 <ankitujjwalkujur14@gmail.com>
2026-07-08 15:31:18 +0530

    chap 7 added

---
+++ chap7.py
@@ -0,0 +1,20 @@
+# Loops
+
+"""
+Loops let you repeat a block of code without rewriting it.
+
+ * Python has 2 types of loops: for and while.
+"""
+
+""" 
+The Bucket Analogy 🪣
+
+Two buckets, one mug — which loop do you use?
+
+* 🔢 FOR loop — known iterations
+Transfer exactly 4 mugs. You know the count → use for.
+
+* 🔁 WHILE loop — known condition
+Transfer until bucket is empty. You don't know the count, but you know when to stop → use while.
+
+"""
