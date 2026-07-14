5d0c79b33f467e0a213e54401e376d735e92bc18
ankitkujur15 <ankitujjwalkujur14@gmail.com>
2026-07-14 15:32:16 +0530

    added chapter 11

---
+++ chap11.py
@@ -0,0 +1,17 @@
+
+# Data structures
+
+"""
+The 4 Built-in Data Structures
+* When you need to store multiple values in one variable, you use a data structure. 
+* Python gives you 4 ready to use:
+
+Structure	Ordered?	Mutable?	Duplicates? 	Access by
+List	    ✅ Yes	   ✅ Yes	  ✅ Yes	        Index
+Tuple	    ✅ Yes	   ❌ No	      ✅ Yes	        Index
+Set	        ❌ No	   ✅ Yes	  ❌ No	        Methods
+Dictionary	✅ Yes	   ✅ Yes	  Keys: ❌	    Key
+
+
+
+"""
