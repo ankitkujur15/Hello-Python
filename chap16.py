89960c69066ddeee00c49e6376069484bc6506f4
ankitkujur15 <ankitujjwalkujur14@gmail.com>
2026-07-20 16:57:50 +0530

    exception handling added

---
+++ chap16.py
@@ -0,0 +1,45 @@
+# Exception handling in python
+
+"""
+Errors vs Exceptions
+❌ Errors (unfixable)
+SyntaxError — wrong syntax
+IndentationError — bad spacing
+TabError — mixing tabs/spaces
+
+✅ Exceptions (handleable!)
+ZeroDivisionError
+TypeError
+ValueError
+FileNotFoundError
+
+* Exceptions can be handled using > try / except / else / finally / raise
+"""
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+try:                                                                # catches error and allows you to move forward with the remaing code
+    print(a//b)
+except Exception as err:
+    print(f"Sorry an error occcured at {err}")
+else:                                                               # have the least priority runs if except fails
+    print("No errors occured")
+finally:                                                                                    # run no matter what
+    print(" if there are errors or not This block will for sure run")
+
+
+name = input("Enter your name: ")
+print(f"Hello my name is {name}")
+
+
+
+
+# raise -> is used to generate manuel errors
+
+age = int(input("enter your age : "))
+
+if age < 18:
+    raise TypeError("Sorry you are not eligible to vote")
+else:
+    print("You are eligible to vote")
